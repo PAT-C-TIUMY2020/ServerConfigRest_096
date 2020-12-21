415208333a12c2584d7ada4c9c26ab383f991191
UC8096 <vyaninsya.nurmuhammad@gmail.com>
2020-12-21 20:44:15 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using ServiceRest_096_Vyan_Insya_Nur_Muhammad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConfigRest_096_Vyan_Insya_Nur_Muhammad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server Ready");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
